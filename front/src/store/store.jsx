cf4d15aeaef0d9bbda3993e66365ef1df8703d5b
Boutzi <joe@arkaans.com>
2024-08-27 17:51:49 +0200

    feat (p10): middleware implementation

--- front/src/store/store.jsx
+++ front/src/store/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer, { login, setUser } from "./authSlice";
-import { fetchUser } from "../services/userServices";
+import authReducer from "./authSlice";
+import { initializeAuth } from "./authSlice";
 
 const store = configureStore({
   reducer: {
@@ -9,25 +9,6 @@ const store = configureStore({
   devTools: process.env.NODE_ENV === "development",
 });
 
-const initializeAuth = async () => {
-  const token = localStorage.getItem("token") || sessionStorage.getItem("token");
-  if (token) {
-    store.dispatch(login({ token }));
-    try {
-      const userData = await fetchUser(token);
-      store.dispatch(setUser({
-        id: userData.id,
-        email: userData.email,
-        userName: userData.userName,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-      }));
-    } catch (error) {
-      console.error("Failed to fetch user data:", error);
-    }
-  }
-};
-
-initializeAuth();
+store.dispatch(initializeAuth());
 
 export default store;
